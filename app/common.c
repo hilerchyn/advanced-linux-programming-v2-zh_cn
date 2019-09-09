5833db2bb7570304bf38001e40cf5554b591c968
Hiler Chen <hilerchyn@gmail.com>
2019-09-10 06:47:09 +0800

    add error function

--- app/common.c
+++ app/common.c
@@ -28,3 +28,32 @@ void *xrealloc(void *ptr, size_t size)
     else
         return ptr;
 }
+
+void system_error(const char *operation)
+{
+    /* generate an error message for errno */
+    error(operation, strerror(errno));
+}
+
+void error(const char *cause, const char *message)
+{
+    /* print an error message to stderr */
+    fprintf(stderr, "%s: error: (%s) %s\n", program_name, cause, message);
+    /* end program */
+    exit(1);
+}
+
+char *get_self_executable_directory()
+{
+    int rval;
+    char link_target[1024];
+    char *last_slash;
+    size_t result_length;
+    char *result;
+
+    rval = readlink("/proc/self/exe", link_target, sizeof(link_target));
+    if (rval == -1)
+        abort();
+    else
+        link_target[rval] = '\0';
+}
